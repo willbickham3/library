c5f35164ac548149c329ed111a28ad4d6861e6f8
Will <willbickham3@gmail.com>
2023-02-28 13:24:03 -0500

    Add new div to library main for each book created, Assign data-attribute to each

--- script.js
+++ script.js
@@ -17,6 +17,10 @@ function addBookToLibrary() {
     let readvalue = document.getElementById('readValue');
     const newBook = new Book(authorvalue.value, titlevalue.value, pagesvalue.value, readvalue.value)
     myLibrary.push(newBook)
+    let i = 0;
+    const newDiv = document.createElement('div');
+    newDiv.dataset.number = (myLibrary.length - 1);
+    document.querySelector('.library-main').appendChild(newDiv)
 }
 
 let form = document.querySelector('#form');
